8e44fd9a46b5507802db50257ee6357ec6cde56c
Livingstone David Kwame Hini <kwamelivingstone77@gmail.com>
2024-02-25 20:55:34 -0500

    feat:changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logomain.png';
+import navicon0 from '../assets/img/navicon0.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/navicon4.png';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -51,11 +53,12 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navicon0} alt="" style={{width: 30}}/></a>
+                <a href="#"><img src={navIcon4} alt="" style={{width: 30}}/></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>View Resume/CV</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
