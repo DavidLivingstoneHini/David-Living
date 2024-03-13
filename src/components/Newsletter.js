b0d15e04fb341f7eb5dfa735984f194f199bddef
Livingstone David Kwame Hini <kwamelivingstone77@gmail.com>
2024-03-13 19:55:37 -0400

    feat:updates

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import EmailIcon from '@mui/icons-material/Email';
 
 export const Newsletter = ({ status, message, onValidated }) => {
   const [email, setEmail] = useState('');
@@ -22,17 +24,34 @@ export const Newsletter = ({ status, message, onValidated }) => {
   }
 
   return (
-      <Col lg={12}>
-        <div className="newsletter-bx wow slideInUp">
-          <Row>
-            <Col lg={12} md={6} xl={5}>
-              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>} 
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
-            </Col>
-          </Row>
-        </div>
-      </Col>
+    <Col lg={12}>
+      <div className="newsletter-bx wow slideInUp">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            {/* <h5 style={{ textAlign: "center" }}>You can also reach me on...</h5> */}
+            {status === 'sending' && <Alert>Sending...</Alert>}
+            {status === 'error' && <Alert variant="danger">{message}</Alert>} 
+            {status === 'success' && <Alert variant="success">{message}</Alert>}
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col lg={6} md={6} xl={5}>
+            <div className="contact-info">
+              <div className="contact-icon">
+                <i className="fa fa-phone"></i>
+              </div>
+              <div className="contact-details">
+              <p><EmailIcon /><a href="mailto:kwamelivingstone77@gmail.com" style={{fontSize: 17, color: "#919191"}}>kwamelivingstone77@gmail.com</a></p>
+              </div>
+            </div>
+          </Col>
+          <Col lg={6} md={6} xl={5}>
+              <div className="contact-details">
+              <p><PhoneInTalkIcon /><a href="tel:+233202651659" style={{fontSize: 16, color: "#919191"}}>+233 2026 51659</a></p>
+              </div>
+          </Col>
+        </Row>
+      </div>
+    </Col>
   )
 }
