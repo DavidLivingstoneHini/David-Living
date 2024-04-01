9386229bb16f7d4c32737f412f921521bb09b73f
Livingstone David Kwame Hini <kwamelivingstone77@gmail.com>
2024-03-31 22:47:49 -0400

    feat:changes

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Col, Row, Alert } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 import EmailIcon from '@mui/icons-material/Email';
 
@@ -27,7 +27,13 @@ export const Newsletter = ({ status, message, onValidated }) => {
     <Col lg={12}>
       <div className="newsletter-bx wow slideInUp">
         <div className="justify-content-center" style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <p style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}><EmailIcon style={{ color: "#CC02C2" }} /><span style={{marginLeft: 7}}></span><a href="mailto:kwamelivingstone77@gmail.com" style={{ fontSize: 16, color: "#CC02C2" }}>kwamelivingstone77@gmail.com</a></p>
+          <p style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center', margin: '0 auto' }}>
+            <EmailIcon style={{ color: "#CC02C2" }} />
+            <span style={{ marginLeft: 7 }}></span>
+            <a href="mailto:kwamelivingstone77@gmail.com" style={{ fontSize: 16, color: "#CC02C2", display: 'block', textAlign: 'center' }}>
+              <span style={{ fontSize: '16px', '@media (max-width: 768px)': { fontSize: '12px' } }}>kwamelivingstone77@gmail.com</span>
+            </a>
+          </p>
         </div>
       </div>
     </Col>
